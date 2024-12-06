04fe444fb9492dea54fb516415ef86c13e16bd18
C.SREERAG <99243618+SreeRag1907@users.noreply.github.com>
2024-12-06 21:37:11 +0530

    addded abou section

--- gaming-site/src/App.jsx
+++ gaming-site/src/App.jsx
@@ -1,3 +1,4 @@
+import About from "./components/About"
 import Hero from "./components/Hero"
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden"> 
       <Hero/>
+      <About/>
     </main>
   )
 }
